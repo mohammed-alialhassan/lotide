6c2608d395b0ddc21c5364d367c31f2dfc752e1d
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-13 23:27:12 +0000

    head.js

---
+++ head.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😎😎😎 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😓😓😓 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function assert(actual, expected ) {
+return actual[0]
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([3]),3);
+assertEqual(head([ ]), 8);
+
+
+
+
+
