10a49a6122630b64a4644b031e56d1b4c23dcd2c
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-13 23:22:03 +0000

    changes made to assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,14 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("😎😎😎 Assertion Passed: " + actual + " === " + expected);
+    console.log(`😎😎😎 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("😓😓😓 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`😓😓😓 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
