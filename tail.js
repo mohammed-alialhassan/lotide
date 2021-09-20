402da774b7b975ff5a411a9810060ddfe8a2f4c7
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-20 21:53:48 +0000

    refactor changes made and tailtest.js

--- tail.js
+++ tail.js
@@ -1,16 +1,22 @@
 const assertEqual = require('./assertEqual');
 
-const tail = function(array) {
-  return array.slice[1];
+const tail = function(arr) {
+  let head = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== 0) {
+      head.push(arr[i]);
+    }
+  }
+  return head;
 };
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
+console.log(tail(words));
 assertEqual(words.length, 3);
-
+/*
 const example1 = [5];
 assertEqual(example1.length, 1);
 const example2 = [];
 assertEqual(example2.length, 0);
-
-module.exports = tail
+*/
+module.exports = tail 
