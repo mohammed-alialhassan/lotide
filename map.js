28c6140a9e7aeb528b74091f0aa51248820cade8
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-20 23:23:25 +0000

    refactor and changes/module added to

--- map.js
+++ map.js
@@ -1,4 +1,6 @@
-const words = ["ground", "beef", "spaghetti", "meatballs", "yummy", "krabby", "patty"];
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 //Map function has two parameters with the SPECIFIC arguements of array and callbackFunction
 
@@ -10,43 +12,5 @@ const map = function(array, callback) {
   }
   return results;
 };
-const results1 = map(words, word => word[0]);
-console.log(results1);
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😓😓😓 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length >= arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else if (arr1.length < arr2.length) {
-    for (let i = 0; i < arr2.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-assertEqual(eqArrays([1, 2, 3,], [1, 2, 3,]), true);
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1,arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else if (!eqArrays(arr1, arr2)) {
-    console.log(`😓😓😓 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-assertArraysEqual(results1, ["g", "b", "s", "m", "y", "k", "p"]);
+module.exports = map
