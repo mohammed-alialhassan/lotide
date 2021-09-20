2119a030818bfd7f986c7872abffee42a993ba39
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-20 23:09:38 +0000

    refactor changes made

--- assertEqual.js
+++ assertEqual.js
@@ -1,29 +1,11 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`😎😎😎 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`😓😓😓 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("you", "you");
-assertEqual("you", "making");
-assertEqual(12, 12);
-assertEqual(30, 35);
-
+module.exports = assertEqual
 
 
