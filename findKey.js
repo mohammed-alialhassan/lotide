9f4c4daad8405e5523e017e35e5320bb68fce431
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-20 23:16:14 +0000

    refactor changes made to

--- findKey.js
+++ findKey.js
@@ -1,19 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😓😓😓 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const object = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri": { stars: 3 },
-  "noma": { stars: 2 },
-  "elBulli": { stars: 3 },
-  "Ora": { stars: 2 },
-  "Akelarre": { stars: 3 }
-};
+const assertEqual = require('./assertEqual');
 
 //need to make a function called findKey, needs 2 parameters: object and callback
 
@@ -28,13 +13,7 @@ const findKey = function (object, callback) {
   }
 };
 
-const results1 = findKey(object, x => x.stars === 2);
-
-const results2 = findKey(object, x => x.stars === 0);
-
-assertEqual(results1, "noma");
-
-assertEqual(results2, undefined);
+module.exports = findKey
 
 
 
