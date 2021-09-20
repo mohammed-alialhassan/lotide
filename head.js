431883aa083dd2a4696a4820c8becac0efbb5388
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-20 22:03:47 +0000

    refactor changes made

--- head.js
+++ head.js
@@ -1,21 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😎😎😎 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😓😓😓 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function (array) {
 return array[0]
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([3]),3);
-assertEqual(head([ ]), 8);
-
-
+module.exports = head
 
 
 
