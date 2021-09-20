67499a03c6a6eff518a842a3f8dc47450549b600
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-20 23:18:53 +0000

    refactor and module.exports added

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,12 +1,6 @@
 const { object } = require("joi");
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😎😎😎 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😓😓😓 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 
 const findKeyByValue = function(object, value) { //function with parameters object and value
@@ -18,11 +12,5 @@ const findKeyByValue = function(object, value) { //function with parameters obje
        } //else will return undefined
      }
 }
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue
