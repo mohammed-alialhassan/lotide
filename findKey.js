978f8ddbb0305c6079eb0fc3fa285fa4773b47a8
Mohammed Ali Al-Hassan <88508812+mohammed-alialhassan@users.noreply.github.com>
2021-09-21 18:24:01 -0400

    Update findKey.js

--- findKey.js
+++ findKey.js
@@ -3,7 +3,6 @@ const assertEqual = require('./assertEqual');
 //need to make a function called findKey, needs 2 parameters: object and callback
 
 const findKey = function (object, callback) {
-  let result = undefined;
   //It needs to scan the object's keys (for in loop)
   for (let key in object) {
 //the FIRST key that returns true with the callback function should be returned
